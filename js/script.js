250d48c4dd2161f98dd9263ebf8ed1cf98f4c49b
RobertoPedral <dev.robertopedral@gmail.com>
2023-01-21 00:27:56 -0300

    questao2(issue2)

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
-var lado1 = document.querySelector("#lado1");
-var lado2 = document.querySelector("#lado2");
-var lado3 = document.querySelector("#lado3");
-var resultado = document.querySelector("#resultado");
-
-function tipo() {
-  if (
-    parseFloat(lado1.value) >
-      parseFloat(lado2.value) + parseFloat(lado3.value) ||
-    parseFloat(lado2.value) >
-      parseFloat(lado1.value) + parseFloat(lado3.value) ||
-    parseFloat(lado3.value) > parseFloat(lado1.value) + parseFloat(lado2.value)
-  ) {
-    resultado.innerHTML = "Não se pode formar um triângulo";
+function fibonacci() {
+  var termos = document.querySelector("#numero").value;
+  var resultado = document.querySelector("#resultado");
+  if (termos <= 0) {
+    resultado.innerHTML = "Número de termos inválidos!";
   } else {
-    if (
-      parseFloat(lado1.value) == parseFloat(lado2.value) &&
-      parseFloat(lado1.value) == parseFloat(lado3.value)
-    ) {
-      resultado.innerHTML = "Triângulo Equilátero";
-    } else if (
-      parseFloat(lado1.value) == parseFloat(lado2.value) ||
-      parseFloat(lado1.value) == parseFloat(lado3.value) ||
-      parseFloat(lado2.value) == parseFloat(lado3.value)
-    ) {
-      resultado.innerHTML = "Triângulo Isórceles";
-    } else {
-      resultado.innerHTML = "Triângulo Escaleno";
+    var num1 = 1,
+      num2 = 0,
+      num3 = 0,
+      numx = 0;
+    var array = [];
+    for (let i = 0; i < termos; i++) {
+      array.push(num3);
+      num3 = num1 + num2;
+      numx = num2 / num1;
+      num1 = num2;
+      num2 = num3;
     }
+    resultado.innerHTML = array;
   }
 }
